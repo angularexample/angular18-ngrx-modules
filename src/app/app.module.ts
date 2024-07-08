c92a2c76388d801fdd7e51f9242c476f293a48e1
JC Lango <jobs@jclango.com>
2024-07-08 14:55:32 -0400

    fixed missing state in reduxdevtools, use module code from official docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {BrowserModule} from '@angular/platform-browser';
 import {EffectsModule} from '@ngrx/effects';
-import {environment} from '../environments/environment';
-import {NgModule} from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
@@ -20,8 +19,15 @@ import {XxxHeaderModule} from "./xxx-header/xxx-header.module";
     BrowserModule,
     BrowserAnimationsModule,
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true // If set to true, the connection is established within the Angular zone
+    }),
     XxxCommonModule,
     XxxHeaderModule,
   ],
